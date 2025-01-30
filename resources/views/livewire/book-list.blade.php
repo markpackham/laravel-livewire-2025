972c33771e830a09b6a6836d98a96fbaab1ea8bf
M <markpackham1@gmail.com>
2025-01-30 15:30:31 +0000

    Change name dynamically with time delay in form
    https://youtu.be/MzziADLTiE4?si=4yKlUCQ4mHhvuZ7i&t=36

--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -14,9 +14,18 @@
 
     {{-- Does same as above but neater focusing on form's submit --}}
     {{-- We change the once hard coded "name" to whatever we enter in the form --}}
-    <form wire:submit="$refresh">
+    {{-- <form wire:submit="$refresh">
       <span class="mr-2">Your Name: </span>
       <input type="text" wire:model="name">
+      <button>Update</button>
+    </form> --}}
+
+    {{-- Same as above but modifies "name" .live after every keystroke --}}
+    {{-- If we want to save on resources we add ".debounce" so data is entered only after a set time
+    that we stopped typing eg 500 milliseconds --}}
+    <form wire:submit="$refresh">
+      <span class="mr-2">Your Name: </span>
+      <input type="text" wire:model.live.debounce.500ms="name">
       <button>Update</button>
     </form>
 
