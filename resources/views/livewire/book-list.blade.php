485fb1442d813df672cc6c61e79024307bbff73e
M <markpackham1@gmail.com>
2025-01-30 11:43:53 +0000

    Layout book-list

--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -1,3 +1,18 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+  <header class="flex justify-between">
+    <div>
+      <h2>Hi, {{ $name }}</h2>
+      <p>Here's a list of your book reviews...</p>
+    </div>
+  </header>
+
+  <ul class="list">
+    @foreach($books as $book)
+      <li wire:key="{{ $book->id }}">
+        <h3>{{ $book->title }}</h3>
+        <h4>{{ $book->author }}</h4>
+        <p>Rating: {{ $book->rating }}/10</p>
+      </li>
+    @endforeach
+  </ul>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Livewire Crash Course</title>
-  
+
   @vite('resources/css/app.css')
 </head>
 <body>
   <main>
     <h2>Welcome to the Livewire Crash Course!</h2>
+    <livewire:book-list>
   </main>
 </body>
 </html>
