f44734868a43f5925ebbf14217b21f95205f37ce
M <markpackham1@gmail.com>
2025-01-31 10:24:54 +0000

    Remove secondary layout only intended for demo purposes
    https://youtu.be/rYZHH49F7OA?feature=shared&t=17

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -2,13 +2,10 @@
 
 namespace App\Livewire;
 
-use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class CreateBook extends Component
 {
-    // Use attribute so second.blade.php layout used rather than main app.blade.php
-    #[Layout('components.layouts.second')]
     public function render()
     {
         return view('livewire.create-book');
