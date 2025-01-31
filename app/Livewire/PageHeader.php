25ef5820fa3b1ff6072d9f485115225b4feb68a7
M <markpackham1@gmail.com>
2025-01-31 10:52:26 +0000

    Demo use of mount hook and a prop for subtitles
    https://youtu.be/xJOawqIe9CA?feature=shared&t=218

--- app/Livewire/PageHeader.php
+++ app/Livewire/PageHeader.php
@@ -8,7 +8,14 @@ class PageHeader extends Component
 {
 
     public $name = 'Mario';
+    public $subtitle;
 
+    // Life cycle hooks, mount, updated & rendering are common
+    // mount runs when component first created
+    public function mount($subtitle){
+        // Use whatever prop value got passed into it from book-list.blade.php
+        $this->subtitle = $subtitle;
+    }
 
     public function render()
     {
