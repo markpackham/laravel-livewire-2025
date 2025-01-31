9a9e7edab8754681ab8215bafe25dcea8a786545
M <markpackham1@gmail.com>
2025-01-31 10:02:21 +0000

    php artisan livewire:layout Make the default Livewire layout & add a page title
    https://youtu.be/MzziADLTiE4?feature=shared&t=307

--- app/Livewire/BookList.php
+++ app/Livewire/BookList.php
@@ -3,15 +3,20 @@
 namespace App\Livewire;
 
 use App\Models\Book;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class BookList extends Component
 {
   public $name = 'Mario';
 
-  public function delete(Book $book){
+  public function delete(Book $book)
+  {
     $book->delete();
   }
+
+  // Appears in title of html in main layout
+  #[Title('Book List - Home')]
 
   public function render()
   {
