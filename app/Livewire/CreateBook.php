20aa341479bd06ba02b6e11e06bb7474523d41de
M <markpackham1@gmail.com>
2025-02-03 15:18:06 +0000

    Demo validation the Livewire way
    https://youtu.be/KD2whSRsMtg?si=_rqrYZTyCAX8cQxX&t=443

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -3,23 +3,34 @@
 namespace App\Livewire;
 
 use App\Models\Book;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class CreateBook extends Component
 {
+    // How to do validation the Livewire way
+    #[Rule('string|required|min:3|max:300')]
     public $title;
+
+    #[Rule('string|required|min:3|max:300')]
     public $author;
+    
+    #[Rule('integer|required|min:1|max:10')]
     public $rating;
 
     public function save()
     {
 
-        $this->validate([
-            'title' => 'string|required|min:3|max:300',
-            'author' => 'string|required|min:3|max:300',
-            'rating' => 'integer|required|min:1|max:10',
+        // Old validation method done in Laravel instead of the Livewire way
+        // $this->validate([
+        //     'title' => 'string|required|min:3|max:300',
+        //     'author' => 'string|required|min:3|max:300',
+        //     'rating' => 'integer|required|min:1|max:10',
+        // ]);
 
-        ]);
+        // If we use the validate method without arguments Livewire evaluates each property
+        // based on the rules we defined for them above
+        $this->validate();
 
         Book::create([
             'title' => $this->title,
